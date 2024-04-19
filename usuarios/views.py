debe1a99c8afb26dacbe7a45d8d796be9127b714
Gutemberg Monteiro Regadas <guttoregadas@hotmail.com>
2024-04-19 13:43:17 -0300

    version002

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            print('Erro 2')
+            return redirect('/usuarios/cadastro')
+        
+        if senha < 6:
+            print('Erro 3')
+            return redirect('/usuarios/cadastro')
+
+        return HttpResponse(f"{username} - {email} - {senha} - {confirmar_senha}")
+
