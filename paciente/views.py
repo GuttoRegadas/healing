1e120bb4f0bb510172c1a2f40be39f18003a0264
Gutemberg Monteiro Regadas <guttoregadas@hotmail.com>
2024-06-05 16:30:32 -0300

    version016

--- paciente/views.py
+++ paciente/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from medico.models import DadosMedico
 
 # Create your views here.
 def home(resquest):
     if resquest.method == "GET":
-
-        return render(resquest, 'home.html')
+        medicos = DadosMedico.objects.all()
+        return render(resquest, 'home.html', {'medicos': medicos})
