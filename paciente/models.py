be4fa3fa9d818e188c5119e93cb0220f61ef34ce
Gutemberg Monteiro Regadas <guttoregadas@hotmail.com>
2024-06-18 09:14:20 -0300

    version017

--- paciente/models.py
+++ paciente/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from medico.models import DatasAbertas
 
 # Create your models here.
+
+
+class Consulta(models.Model):
+    status_shoices = (
+        ('A', 'Agendado'),
+        ('F', 'Finalizada'),
+        ('C', 'Cancelada'),
+        ('I', 'Iniciada'),
+    )
+    paciente = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data_aberta = models.ForeignKey(DatasAbertas, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=status_shoices, default='A')
+    link = models.URLField(null=True, blank=True)
