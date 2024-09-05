d826a88943c2871a808be5dba0b7da5f7ffe8327
Gutemberg Monteiro Regadas <guttoregadas@hotmail.com>
2024-09-05 08:29:57 -0300

    version022

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'usuarios',
     'medico',
     'paciente',
+    'menusites',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('medicos/', include('medico.urls')),
     path('pacientes/', include('paciente.urls')),
+    path('menusites/', include('menusites.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
